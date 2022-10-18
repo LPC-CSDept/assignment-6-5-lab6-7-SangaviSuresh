ad426e131faf1183344d85008fa7c684625d3fa7
SangaviSuresh <111937090+SangaviSuresh@users.noreply.github.com>
2022-10-18 21:48:05 +0000

    a65

--- A65.hpp
+++ A65.hpp
@@ -1,11 +1,24 @@
-//
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
 
+void swaptwo(int&, int&);
+void swap(int&, int&, int&);
+
 // complete two functions
 // swap() ; to swap all three values like (a, b, c) to (c, a, b)
 //              This function will use swaptwo().
+void swap(int &a, int &b, int &c)
+{
+    string result;
+    swaptwo(a, b);
+    swaptwo(a, c);
+
+}
 
 // swaptwo() ; to swap two values (a, b) to (b, a)
+void swaptwo(int &A, int &B)
+{
+    int temp = A;
+    A = B;
+    B = temp;
